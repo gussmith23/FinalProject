efee172bdb5cde8a048d9d0adb7cfb385c8c35dc
Gus Smith <hfs5022@psu.edu>
2014-02-10 19:55:05 -0500

    Add switch statement

---
+++ FinalProject/Source.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+	int userArg = atoi(argv[1]);
+
+	//1-1: main function switch
+	switch(userArg){
+		//load document
+		case 0:
+		break;
+
+		//output document
+		case 1: 
+		break;
+
+		//parse document
+		case 2: 
+		break;
+
+		//analyze document
+		case 3:
+		break;
+
+		//exit
+		case 4:
+		break;
+	}
+
+}
