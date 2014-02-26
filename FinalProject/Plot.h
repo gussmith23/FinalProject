f882ffcbd4a085063b49467175f1a87c4ba7b034
Gus Smith <hfs5022@psu.edu>
2014-02-26 14:14:23 -0500

    Finishes all objectives

---
+++ FinalProject/Plot.h
@@ -0,0 +1,35 @@
+/*1-13 Create a Class called Plot. Private: int id, array of string xlabel, 
+array of string ylabels, int numPoints. Make the correct setters and getters.*/
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Plot{
+	public:
+
+		//getters and setters
+		int getId() const;
+		void setId(int);
+
+		vector<string> getXLabel()const;
+		void setXLabel(vector<string>);
+		void addXLabel(string);
+
+		vector<string> getYLabel()const;
+		void setYLabel(vector<string>);
+		void addYLabel(string);
+
+		int getNumPoints() const;
+		void setNumPoints(int);
+
+		//functions
+		//1-14
+		void plot2D(double,double, int,int);
+
+	private:
+		int id;
+		vector<string> xlabel;
+		vector<string> ylabel;
+		int numPoints;
+};
